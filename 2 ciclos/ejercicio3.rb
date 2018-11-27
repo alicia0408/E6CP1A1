1d471d40515339d7e5a9ff2dcea2a41955faad4d
Alicia <tucorreo@mail.com>
2018-11-26 21:26:48 -0300

    ejercicio 3 con for

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,11 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+for i in 1..990
+   puts i if (990 % 1).zero?
+end
+#while i  (990%1).zero?
+ #   puts i
+#end
+#990.times do |i|
+#   puts i  (990 % 1).zero?
+#end
