6a07ea2dc0bc5f3ac148f23be25beb7cc9d7f523
Alicia <tucorreo@mail.com>
2018-12-02 14:40:27 -0300

    resolucion ejercicio 8

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -1,9 +1,14 @@
 # Generar -utilizando un ciclo iterativo- un string con la siguiente estructura:
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
-a = ''
+a = " "
 10.times do |i|
-	
+i += 1
+if i.odd?
+    a += "#{i}impar "
+  else
+    a += "#{i}par "
+  end
 end
 
 puts a
